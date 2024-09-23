4ac353529e046881fc53b17eb621bc518ef5ed15
kyden <kytedance@gmail.com>
2024-09-23 23:19:38 +0800

    feat(nvim): support nvim

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+require("core.keymaps")
+require("core.options")
+require("core.colorscheme")
+
+
+require("plugins")
+
---
+++ nvim/lua/core/colorscheme.lua
@@ -0,0 +1,9 @@
+-- define your colorscheme here
+local colorscheme = 'gruvbox'
+
+local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not ok then
+    vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+    return
+end
+
